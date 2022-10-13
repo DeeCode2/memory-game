f986e2616c02ae381e213ce63fa55e88ca1a53db
DeeCode2 <jellyfrog025@gmail.com>
2022-10-14 00:13:30 +0100

    cleanup and background blur filter

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //get all cards
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll(".card");
 
 //matching cards
 let hasFlipped = false; //initially none of the cards are flipped and so the default is false
@@ -9,79 +9,80 @@ let count = 0;
 
 //function to flip card
 function flipCard() {
-    if (lockBoard) return;
-    if (this === firstCard) return;
+  if (lockBoard) return;
+  if (this === firstCard) return;
 
-    this.classList.add('flip');
+  this.classList.add("flip");
 
-    if (!hasFlipped) { //!hasFlipped = opposite of false aka "if this card has been clicked" - will always be true when the function is clicked
-        hasFlipped = true;
-        firstCard = this;
-        return;
-    }
+  if (!hasFlipped) {
+    //!hasFlipped = opposite of false aka "if this card has been clicked" - will always be true when the function is clicked
+    hasFlipped = true;
+    firstCard = this;
+    return;
+  }
 
-    secondCard = this;
-    matchCard();
+  secondCard = this;
+  matchCard();
 }
 
 //function to check if flipped cards match by comparing data attributes
 function matchCard() {
-    //flip or freeze card pairs depending on whether or not they match
-    let cardMatch = firstCard.dataset.animal === secondCard.dataset.animal;
-    cardMatch ? freezeCards() : unflip();
+  //flip or freeze card pairs depending on whether or not they match
+  let cardMatch = firstCard.dataset.animal === secondCard.dataset.animal;
+  cardMatch ? freezeCards() : unflip();
 }
 
 //function to disable cards by making then unclickable
 function freezeCards() {
-    firstCard.removeEventListener('click', flipCard);
-    firstCard.classList.add('flipped') //keep track of the matched cards
-    
-    secondCard.removeEventListener('click', flipCard);
-    secondCard.classList.add('flipped');
+  firstCard.removeEventListener("click", flipCard);
+  firstCard.classList.add("flipped"); //keep track of the matched cards
 
-    count += 20;
-    console.log(count)
+  secondCard.removeEventListener("click", flipCard);
+  secondCard.classList.add("flipped");
 
-    //check if all cards are flipped
-    if (document.querySelectorAll('.flipped').length === 16) {
-        //end the game when all cards are flipped
-        const modal = document.getElementById('modal');
-        const score = document.getElementById('score');
-        finalScore = String(count)
-        modal.style.visibility = 'visible';
-        score.innerText = `Score: ${finalScore}`
-    } else {
-        //subtract points and continue playing
-        console.log("some cards are still unmatched")
-        resetGrid();
-    }
+  count += 20;
+  console.log(count);
+
+  //check if all cards are flipped
+  if (document.querySelectorAll(".flipped").length === 16) {
+    //end the game when all cards are flipped
+    const modal = document.getElementById("modal");
+    const score = document.getElementById("score");
+    finalScore = String(count);
+    modal.style.visibility = "visible";
+    score.innerText = `Score: ${finalScore}`;
+  } else {
+    //subtract points and continue playing
+    console.log("some cards are still unmatched");
+    resetGrid();
+  }
 }
 
 //function to unflip cards if they dont match
 function unflip() {
-    lockBoard = true;
-    setTimeout(() => {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-        resetGrid();
-    }, 1400); //wait before unflipping cards
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    resetGrid();
+  }, 1400); //wait before unflipping cards
 }
 
 //make all cards clickable again by setting the card and board to initial values
 function resetGrid() {
-    [hasFlipped, lockBoard] = [false, false];
-    [firstCard, secondCard] = [null, null];
-    count -=3;
-    console.log(count);
+  [hasFlipped, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+  count -= 3;
+  console.log(count);
 }
 
 //shuffle card order by changing flex item order
 (function shuffle() {
-    cards.forEach(card => {
-      let ramdomPos = Math.floor(Math.random() * 12);
-      card.style.order = ramdomPos;
-    });
+  cards.forEach((card) => {
+    let ramdomPos = Math.floor(Math.random() * 12);
+    card.style.order = ramdomPos;
+  });
 })(); //function is an immediately invoked function and shuffles the card after loading
 
 //add eventlistener to all cards on grid
-cards.forEach(card => card.addEventListener('click', flipCard));
+cards.forEach((card) => card.addEventListener("click", flipCard));
